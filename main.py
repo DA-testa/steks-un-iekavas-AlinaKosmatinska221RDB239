5a3e4ac1531eec2c9f799b55b2ce8e9b1cbe1587
AlinaKosmatinska221RDB239 <125760180+AlinaKosmatinska221RDB239@users.noreply.github.com>
2023-02-20 03:17:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    if text[0]=="i":
+    if text[0]=="I":
         text=input()
         mismatch = find_mismatch(text)
     # Printing answer, write your code here
