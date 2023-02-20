a6412dba1cc415a287e2f571a542fa85e3ba5b64
AlinaKosmatinska221RDB239 <125760180+AlinaKosmatinska221RDB239@users.noreply.github.com>
2023-02-20 03:08:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,42 @@
 
 from collections import namedtuple
 
-#Bracket = namedtuple("Bracket", ["char", "position"])
+Bracket = namedtuple("Bracket", ["char", "position"])
 
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 def find_mismatch(text):
-    opening_brackets_stack = []
-    opening_brackets_position = {}
+    #opening_brackets_stack = []
+    #opening_brackets_position = {}
     for i, next in enumerate(text):
         if next in '([{':
             # Process opening bracket, write your code here)
-            #opening_brackets_stack.append(Bracket(next, i+1))
-            opening_brackets_stack.append(next)
+            opening_brackets_stack.append(Bracket(next, i+1)) #pievieno vienu vērtību
+            #opening_brackets_stack.append(next)
             opening_brackets_position[next]=i+1
             pass
 
        
             # Process closing bracket, write your code here
-        elif next in ')]}':
-            if len(openiing_brackets_stack)==0:
-                return i+1
-            opening_brackets=opening_brackets_stack.pop()
-            if (next==')'and opening_brackets != '(') or \
-               (next==']'and opening_brackets != '[') or \
-               (next=='}'and opening_brackets != '{'):
-                        return i + 1, pening_brackets_position[opening_brackets]
-    if len(opening_brackets_stack)==0:
+        #elif next in ')]}':
+            #if len(openiing_brackets_stack)==0:
+                #return i+1
+            #opening_brackets=opening_brackets_stack.pop()
+            #if (next==')'and opening_brackets != '(') or \
+               #(next==']'and opening_brackets != '[') or \
+               #(next=='}'and opening_brackets != '{'):
+                        #return i + 1, opening_brackets_position[opening_brackets]
+    #if len(opening_brackets_stack)==0:
         #return opening_brackets_stack[0].position
-        return "Success"
-    else:
-        return opening_brackets_position[opening_brackets_stack.pop()]
+        #return "Success"
+    #else:
+        #return opening_brackets_position[opening_brackets_stack.pop()]
+            if opening_brackets_stack==0 or are_matching==0(opening_brackets_stack.pop().char,next):
+                return i+1
+            if opening_brackets_stack:
+                return opening_brackets_stack[0].position
+            return "Success"
             pass
 
 
